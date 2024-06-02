3b8a621a8dcf85ef7cedaa07a31386a0c5839129
luckenty <luckenty@gmail.com>
2024-06-02 20:57:46 +0300

    refactor MyContainer

--- MyContainer.h
+++ MyContainer.h
@@ -2,18 +2,69 @@
 #define MYCONTAINER_H
 
 template <typename T>
-class MyContainer {
+class MyContainer
+{
 public:
-    void push_back(T value);
-    void insert(int index, T value);
-    void erase(int index);
-    int size();
+    MyContainer(): m_size(0), m_capacity(10), m_data(new T[m_capacity])
+    {
+    }
 
-    T operator[](int index);
+    MyContainer(int size)
+        : m_size(size), m_capacity(size * 2), m_data(new T[m_capacity])
+    {
+    }
+
+    MyContainer(const MyContainer& other)
+        : m_size(other.m_size), m_capacity(other.m_capacity),
+          m_data(new T[m_capacity])
+    {
+        for (int i = 0; i < m_size; i++)
+            m_data[i] = other.m_data[i];
+    }
+
+    ~MyContainer() { delete[] m_data; }
+
+    void push_back(const T& val)
+    {
+        if (m_size >= m_capacity)
+            increase_capacity(m_capacity * 2);
+        m_data[m_size++] = val;
+    }
+
+    void insert(const T& val, int index)
+    {
+        if (index < 0 || index > m_size)
+            return;
+
+        if (m_size >= m_capacity)
+            increase_capacity(m_capacity * 2);
+
+        for (int i = m_size - 1; i >= index; i--)
+            m_data[i + 1] = m_data[i];
+
+        m_data[index] = val;
+        m_size++;
+    }
+
+    void increase_capacity(int newCapacity)
+    {
+        T* newData = new T[newCapacity];
+        for (int i = 0; i < m_size; i++)
+            newData[i] = m_data[i];
+
+        delete[] m_data;
+        m_data = newData;
+        m_capacity = newCapacity;
+    }
+
+    int size() const { return m_size; }
+
+    T& operator[](int index) { return m_data[index]; }
 
 private:
-    int m_size;
-    T m_region;
+    size_t m_size;
+    size_t m_capacity;
+    T* m_data;
 };
 
 #endif //MYCONTAINER_H
--- main.cpp
+++ main.cpp
@@ -2,9 +2,19 @@
 #include <fstream>
 #include <iostream>
 
-int main(int argc, char **argv) {
+#include "MyContainer.h"
 
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char **argv) {
+    MyContainer<int> container;
+
+    container.push_back(10);
+    container.push_back(20);
+    container.push_back(30);
+    container.push_back(40);
+    container.push_back(50);
+    container.push_back(60);
+    std::cout << "size = " << container.size() << std::endl;
+    std::cout << "value at index 3 is: " << container[3] << std::endl;
 
     return 0;
 }
