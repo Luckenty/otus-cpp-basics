7df247a54e1e1a476e66b20d50ff5b9bab0de880
Luckenty <luckenty@gmail.com>
2024-06-10 00:02:41 +0300

    fix

--- MyContainer.h
+++ MyContainer.h
@@ -1,38 +1,32 @@
 #ifndef MYCONTAINER_H
 #define MYCONTAINER_H
 
-template <typename T>
-class MyContainer
-{
+template<typename T>
+class MyContainer {
 public:
-    MyContainer(): m_size(0), m_capacity(10), m_data(new T[m_capacity])
-    {
+    MyContainer() : m_size(0), m_capacity(10), m_data(new T[m_capacity]) {
     }
 
     MyContainer(int size)
-        : m_size(size), m_capacity(size * 2), m_data(new T[m_capacity])
-    {
+            : m_size(size), m_capacity(size * 2), m_data(new T[m_capacity]) {
     }
 
-    MyContainer(const MyContainer& other)
-        : m_size(other.m_size), m_capacity(other.m_capacity),
-          m_data(new T[m_capacity])
-    {
+    MyContainer(const MyContainer &other)
+            : m_size(other.m_size), m_capacity(other.m_capacity),
+              m_data(new T[m_capacity]) {
         for (int i = 0; i < m_size; i++)
             m_data[i] = other.m_data[i];
     }
 
     ~MyContainer() { delete[] m_data; }
 
-    void push_back(const T& val)
-    {
+    void push_back(const T &val) {
         if (m_size >= m_capacity)
             increase_capacity(m_capacity * 2);
         m_data[m_size++] = val;
     }
 
-    void insert(const T& val, int index)
-    {
+    void insert(const T &val, int index) {
         if (index < 0 || index > m_size)
             return;
 
@@ -46,9 +40,8 @@ public:
         m_size++;
     }
 
-    void increase_capacity(int newCapacity)
-    {
-        T* newData = new T[newCapacity];
+    void increase_capacity(int newCapacity) {
+        T *newData = new T[newCapacity];
         for (int i = 0; i < m_size; i++)
             newData[i] = m_data[i];
 
@@ -59,12 +52,12 @@ public:
 
     int size() const { return m_size; }
 
-    T& operator[](int index) { return m_data[index]; }
+    T &operator[](int index) { return m_data[index]; }
 
 private:
     size_t m_size;
     size_t m_capacity;
-    T* m_data;
+    T *m_data;
 };
 
 #endif //MYCONTAINER_H
