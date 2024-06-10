30ed7ba470a377065e9f87bcf23b0a0c16baf38f
Dmitry <dmitry.orlov@raiffeisen.ru>
2024-06-10 22:08:26 +0300

    add erase method

--- MyContainer.h
+++ MyContainer.h
@@ -4,7 +4,7 @@
 template<typename T>
 class MyContainer {
 public:
-    MyContainer() : m_size(0), m_capacity(10), m_data(new T[m_capacity]) {
+    MyContainer() : m_size(0), m_capacity(5), m_data(new T[m_capacity]) {
     }
 
     MyContainer(int size)
@@ -14,45 +14,66 @@ public:
     MyContainer(const MyContainer &other)
             : m_size(other.m_size), m_capacity(other.m_capacity),
               m_data(new T[m_capacity]) {
-        for (int i = 0; i < m_size; i++)
-            m_data[i] = other.m_data[i];
+        for (int i = 0; i < m_size; i++) { m_data[i] = other.m_data[i]; }
     }
 
-    ~MyContainer() { delete[] m_data; }
+    ~MyContainer() {
+        delete[] m_data;
+    }
 
     void push_back(const T &val) {
-        if (m_size >= m_capacity)
+        if (m_size >= m_capacity) {
             increase_capacity(m_capacity * 2);
+        }
         m_data[m_size++] = val;
     }
 
     void insert(const T &val, int index) {
-        if (index < 0 || index > m_size)
+        if (index < 0 || index > m_size) {
             return;
+        }
 
-        if (m_size >= m_capacity)
+        if (m_size >= m_capacity) {
             increase_capacity(m_capacity * 2);
+        }
 
-        for (int i = m_size - 1; i >= index; i--)
+        for (int i = m_size - 1; i >= index; i--) {
             m_data[i + 1] = m_data[i];
+        }
 
         m_data[index] = val;
         m_size++;
     }
 
+    void erase(int index) {
+        if (index < 0 || index > m_size) {
+            return;
+        }
+
+        for (int i = index; i < m_size; i++) {
+            m_data[i] = m_data[i + 1];
+        }
+        m_size--;
+    }
+
     void increase_capacity(int newCapacity) {
         T *newData = new T[newCapacity];
-        for (int i = 0; i < m_size; i++)
+        for (int i = 0; i < m_size; i++) {
             newData[i] = m_data[i];
+        }
 
         delete[] m_data;
         m_data = newData;
         m_capacity = newCapacity;
     }
 
-    int size() const { return m_size; }
+    int size() const {
+        return m_size;
+    }
 
-    T &operator[](int index) { return m_data[index]; }
+    T &operator[](int index) {
+        return m_data[index];
+    }
 
 private:
     size_t m_size;
--- main.cpp
+++ main.cpp
@@ -7,14 +7,28 @@
 int main(int argc, char **argv) {
     MyContainer<int> container;
 
-    container.push_back(10);
-    container.push_back(20);
-    container.push_back(30);
-    container.push_back(40);
-    container.push_back(50);
-    container.push_back(60);
-    std::cout << "size = " << container.size() << std::endl;
-    std::cout << "value at index 3 is: " << container[3] << std::endl;
+    container.push_back(100);
+    container.push_back(200);
+    container.push_back(300);
+    container.push_back(400);
+    container.push_back(500);
+    container.push_back(600);
+    container.insert(123, 3);
+    std::cout << "size before erase = " << container.size() << std::endl;
+    for (int i = 0; i < container.size(); i++) {
+        std::cout << container[i] << " ";
+    }
+    std::cout << std::endl;
+
+    container.erase(2);
+
+    std::cout << "Container after erase:" << std::endl;
+    std::cout << "size after erase = " << container.size() << std::endl;
+    for (int i = 0; i < container.size(); i++) {
+        std::cout << container[i] << " ";
+    }
+    std::cout << std::endl;
+
 
     return 0;
 }
