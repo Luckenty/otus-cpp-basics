99c7e281898b57b560252013598eb2e9ca00c524
Dmitry <dmitry.orlov@raiffeisen.ru>
2024-06-14 17:46:04 +0300

    full implementation of list

--- MyList.h
+++ MyList.h
@@ -11,37 +11,66 @@ public:
         Node *prev;
         T data;
 
-        Node() : next(nullptr), prev(nullptr), data(nullptr) {}
+        Node() : next(nullptr), prev(nullptr), data(0) {}
     };
 
     MyList() : m_first(nullptr), m_last(nullptr), m_size(0) {}
 
     void push_back(T value) {
         Node *new_node = new Node{}; // создание нового узла
-        new_node->prev = m_last; // предыдущим элементом станет последний
-        new_node->next = nullptr; // следующего элемента пока нет
-        new_node->data = value; // сохраняем пользовательские данные
         if (m_first == nullptr) {
             m_first = new_node; // обновляем указатель на первый элемент
             m_last = new_node; // обновляем указатель на последний
+            new_node->data = value; // сохраняем пользовательские данные
+        } else if (m_size == 1) {
+            m_first->next = new_node;
+            new_node->prev = m_last; // предыдущим элементом станет последний
+            new_node->next = nullptr; // следующего элемента пока нет
+            new_node->data = value; // сохраняем пользовательские данные
+            m_last = new_node; // обновляем указатель на последний
         } else {
+            new_node->prev = m_last; // предыдущим элементом станет последний
+            new_node->next = nullptr; // следующего элемента пока нет
+            new_node->data = value; // сохраняем пользовательские данные
+            m_last->next = new_node;
             m_last = new_node; // обновляем указатель на последний
         }
-        m_size += 1; // обновляем размер
+        m_size++; // обновляем размер
     }
 
     void insert(T value, int index) {
-        Node node = this->operator[](index);
+        Node *node = this->operator[](index);
         if (node != nullptr) {
-            Node nodePrev = node.prev;
-            Node nodeNext = node.next;
             Node *new_node = new Node{};
-            new_node->prev = nodePrev;
-            new_node->next = nodeNext;
             new_node->data = value;
-            nodePrev.next = new_node;
-            nodeNext.prev = new_node;
+            Node *nodePrev = node->prev;
+            Node *nodeNext = node->next;
+            if (nodePrev == nullptr) {
+                nodeNext->prev = new_node;
+                new_node->next = node;
+                m_first = new_node;
+            } else if (nodeNext == nullptr) {
+                nodePrev->next = new_node;
+                new_node->prev = nodePrev;
+                m_last = new_node;
+            } else {
+                nodePrev->next = new_node;
+                new_node->prev = nodePrev;
+                new_node->next = node;
+            }
+            m_size++;
+        }
+    }
+
+    void erase(int index) {
+        Node *node = this->operator[](index);
+        if (node != nullptr) {
+            Node *nodePrev = node->prev;
+            Node *nodeNext = node->next;
+            nodePrev->next = nodeNext;
+            nodeNext->prev = nodePrev;
             delete[] node;
+            m_size--;
         }
     }
 
@@ -55,7 +84,7 @@ public:
         return pNode;
     }
 
-    int size() const {
+    [[nodiscard]] int size() const {
         return m_size;
     }
 
--- main.cpp
+++ main.cpp
@@ -4,9 +4,27 @@
 #include "MyContainer.h"
 #include "MyList.h"
 
+template<typename T>
+void print(MyList<T> &container) {
+    for (int i = 0; i < container.size(); i++) {
+        auto node = container[i];
+        std::cout << node->data << " ";
+    }
+    std::cout << std::endl;
+}
+
+template<typename T>
+void print(MyContainer<T> &container) {
+    for (int i = 0; i < container.size(); i++) {
+        std::cout << container[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 void container_test() {
     // 1. создание объекта контейнера для хранения объектов типа int
     MyContainer<int> container;
+
     // 2. добавление в контейнер десяти элементов (0, 1 … 9)
     container.push_back(0);
     container.push_back(1);
@@ -18,57 +36,87 @@ void container_test() {
     container.push_back(7);
     container.push_back(8);
     container.push_back(9);
+
     // 3. вывод содержимого контейнера на экран
-    for (int i = 0; i < container.size(); i++) {
-        std::cout << container[i] << " ";
-    }
-    std::cout << std::endl;
+    print(container);
+
     // 4. вывод размера контейнера на экран
     std::cout << "size = " << container.size() << std::endl;
+
     // 5. удаление третьего (по счёту), пятого и седьмого элементов
     container.erase(2);
     container.erase(3);
     container.erase(4);
 
     // 6. вывод содержимого контейнера на экран
-    for (int i = 0; i < container.size(); i++) {
-        std::cout << container[i] << " ";
-    }
-    std::cout << std::endl;
+    print(container);
 
     // 7. добавление элемента 10 в начало контейнера
     container.insert(10, 0);
 
     // 8. вывод содержимого контейнера на экран
-    for (int i = 0; i < container.size(); i++) {
-        std::cout << container[i] << " ";
-    }
-    std::cout << std::endl;
+    print(container);
 
     // 9. добавление элемента 20 в середину контейнера
     container.insert(20, 4);
 
     // 10. вывод содержимого контейнера на экран
-    for (int i = 0; i < container.size(); i++) {
-        std::cout << container[i] << " ";
-    }
-    std::cout << std::endl;
+    print(container);
 
     // 11. добавление элемента 30 в конец контейнера
     container.push_back(30);
 
     // 12. вывод содержимого контейнера на экран
-    for (int i = 0; i < container.size(); i++) {
-        std::cout << container[i] << " ";
-    }
-    std::cout << std::endl;
+    print(container);
 }
 
 void list_test() {
     // 1. создание объекта контейнера для хранения объектов типа int
     MyList<int> container;
-    // 2. добавление в контейнер десяти элементов (0, 1 … 9)
 
+    // 2. добавление в контейнер десяти элементов (0, 1 … 9)
+    container.push_back(0);
+    container.push_back(1);
+    container.push_back(2);
+    container.push_back(3);
+    container.push_back(4);
+    container.push_back(5);
+    container.push_back(6);
+    container.push_back(7);
+    container.push_back(8);
+    container.push_back(9);
+
+    // 3. вывод содержимого контейнера на экран
+    print(container);
+
+    // 4. вывод размера контейнера на экран
+    std::cout << "size = " << container.size() << std::endl;
+
+    // 5. удаление третьего (по счёту), пятого и седьмого элементов
+    container.erase(2);
+    container.erase(3);
+    container.erase(4);
+
+    // 6. вывод содержимого контейнера на экран
+    print(container);
+
+    // 7. добавление элемента 10 в начало контейнера
+    container.insert(10, 0);
+
+    // 8. вывод содержимого контейнера на экран
+    print(container);
+
+    // 9. добавление элемента 20 в середину контейнера
+    container.insert(20, 4);
+
+    // 10. вывод содержимого контейнера на экран
+    print(container);
+
+    // 11. добавление элемента 30 в конец контейнера
+    container.push_back(30);
+
+    // 12. вывод содержимого контейнера на экран
+    print(container);
 }
 
 int main(int argc, char **argv) {
