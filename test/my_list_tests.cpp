9acade47bd711b71f8fcef035bf21ba749b92a7a
luckenty <luckenty@gmail.com>
2024-06-22 23:44:54 +0300

    add list tests

--- test/my_list_tests.cpp
+++ test/my_list_tests.cpp
@@ -8,3 +8,132 @@ TEST(List, CheckZeroSize) {
 
     EXPECT_EQ(list.size(), expectedZeroSize);
 }
+
+TEST(List, PushBack) {
+    MyList<int> list;
+
+    list.push_back(1);
+
+    const size_t expectedSize = 1;
+
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, Index) {
+    MyList<int> list;
+
+    list.push_back(5);
+
+    const size_t expected = 5;
+
+    EXPECT_EQ(list[0]->data, expected);
+}
+
+TEST(List, InsertAtFirst) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.insert(15,0);
+
+    const size_t expectedSize = 4;
+
+    EXPECT_EQ(list[0]->data, 15);
+    EXPECT_EQ(list[1]->data, 5);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, InsertAtMiddle) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.insert(15,1);
+
+    const size_t expectedSize = 4;
+
+    EXPECT_EQ(list[0]->data, 5);
+    EXPECT_EQ(list[1]->data, 15);
+    EXPECT_EQ(list[2]->data, 6);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, InsertAtEnd) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.insert(15,2);
+
+    const size_t expectedSize = 4;
+
+    EXPECT_EQ(list[2]->data, 15);
+    EXPECT_EQ(list[3]->data, 7);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, EraseFromStart) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.erase(0);
+
+    const size_t expectedSize = 2;
+
+    EXPECT_EQ(list[0]->data, 6);
+    EXPECT_EQ(list[1]->data, 7);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, EraseFromMiddle) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.erase(1);
+
+    const size_t expectedSize = 2;
+
+    EXPECT_EQ(list[0]->data, 5);
+    EXPECT_EQ(list[1]->data, 7);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, EraseFromEnd) {
+    MyList<int> list;
+
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.erase(2);
+
+    const size_t expectedSize = 2;
+
+    EXPECT_EQ(list[0]->data, 5);
+    EXPECT_EQ(list[1]->data, 6);
+    EXPECT_EQ(list.size(), expectedSize);
+}
+
+TEST(List, CheckBigSize) {
+    MyList<int> list;
+
+    for(int i=0; i<100; i++) {
+        list.push_back(i);
+    }
+
+    const size_t expectedSize = 100;
+
+    EXPECT_EQ(list.size(), expectedSize);
+}
